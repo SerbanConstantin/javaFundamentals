d0c196f5dc23205fee8056615bba1cc168e842ee
Serban Constantin <serban.g.constantin@gmail.com>
2019-11-24 14:36:52 +0200

    Punct6

--- src/com/company/Problema12.java
+++ src/com/company/Problema12.java
@@ -25,94 +25,196 @@ import java.util.Scanner;
 
 public class Problema12 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        //PunctA(scan);
-        //PunctB(scan);
-        //PunctC(scan);
-        PunctD(scan);
-    }    public static void PunctA(Scanner scan) {
-        //   1.Write a Java program to sum values of an array. Read the size and read the numbers
-        System.out.print("Dimensiune array(pct1): ");
+        Scanner first = new Scanner(System.in);
+        int optiune;
+        do {
+            System.out.println("1. Write a Java program to sum values of an array. Read the size and read the numbers\n" +
+                    "2. Write a Java program to calculate the average value of array elements\n" +
+                    "3. Write a Java program to remove a specific element from an array. Also change the size of the array\n" +
+                    "4. Write a Java program to insert an element (specific position) into an array\n" +
+                    "5. Write a Java program to find the second largest element in an array.\n" +
+                    "6. Write a Java program to find the number of even and odd integers in a given array of integers\n" +
+                    "0. Exit\n");
+            System.out.print("Please select an option:");
+            optiune = Integer.parseInt(first.next());
+            switch (optiune) {
+                case 1:
+                    Punct1(first);
+                case 2:
+                    Punct2(first);
+                case 3:
+                    punctul3(first);
+                case 4:
+                    punctul4(first);
+                case 5:
+                    Punctul5(first);
+                case 6:
+                    Punctul6(first);
+                default:
+                    System.out.println("Invalid option");
+            }
+        } while (optiune != 0);
+    }
+
+    public static void Punct1(Scanner scan) {
+        System.out.print(" Dimensiune array");
         int dim = scan.nextInt();
         int[] array = readArray(scan, dim);
-        System.out.println("1.Suma este: " + sumaArray(array));
-    }    public static void PunctB(Scanner scan) {
-        //   2.Write a Java program to calculate the average value of array elements
-        System.out.print("Dimensiune array(pct2): ");
+        System.out.println(" Suma elementelor din array: " + sumaArray(array));
+    }
+
+    public static void Punct2(Scanner scan) {
+        //Write a Java program to calculate the average value of array elements
+        System.out.print("Citeste dimensiunea: ");
         int dim = scan.nextInt();
         int[] array = readArray(scan, dim);
-        int avg = sumaArray(array) / dim;
-        System.out.println("2.Average value: " + avg);
-    }    public static void PunctC(Scanner scan) {
-        //   3. Write a Java program to remove a specific element from an array. Also change the size of the array
-        System.out.print("Dimensiune array(pct3): ");
-        int dim = scan.nextInt();
-        int[] array = readArray(scan, dim);
-        System.out.println("Dimens initiala este: " + array.length);
+        System.out.print("Media average este: ");
+        int medie = sumaArray(array) / dim;
+        System.out.println(medie);
+    }
+
+    public static void punctul3(Scanner in) {
+        System.out.println("Dati dimensiune: ");
+        int dim = in.nextInt();
+        int[] array = readArray(in, dim);
+        System.out.println("Dimensiunea initiala este:" + array.length);
         printArray(array);
-        System.out.println();
-        System.out.print("Dati indexul elementului: ");
+        System.out.println("Dati indexul elementului:");
+        int index = in.nextInt();
+        array = extractpossition(array, index);
+        System.out.println("Dimensiunea finala:" + array.length);
+    }
+
+    public static void punctul4(Scanner scan) {
+        //Write a Java program to insert an element (specific position) into an array
+        System.out.println("Dati dimensiune: ");
+        int dim = scan.nextInt();
+        int[] array = readArray(scan, dim);
+        System.out.println("Dimensiunea initiala este:" + array.length);
+        printArray(array);
+        System.out.println("Dati indexul elementului:");
         int index = scan.nextInt();
-        array = extractPosition(array, index);
-        System.out.println("Dimensiunea finala: " + array.length);
+        System.out.println("Dati valoarea: ");
+        int value = scan.nextInt();
+        array = addPosition(array, index, value);
+        System.out.println("Dimensiunea finala:" + array.length);
+        System.out.println();
         printArray(array);
-    }    public static void PunctD(Scanner scan) {
-        //   4.Write a Java program to insert an element (specific position) into an array
-        System.out.print("Dimensiune array(pct4): ");
+    }
+
+    public static void Punctul5(Scanner scan) {
+        // Write a Java program to find the second largest element in an array.
+        System.out.println("Dati dimensiune: ");
         int dim = scan.nextInt();
         int[] array = readArray(scan, dim);
-        System.out.println("Dimens initiala este: " + array.length);
-        printArray(array);
-        System.out.println();
-        System.out.print("Dati indexul elementului: ");
-        int index = scan.nextInt();
-        System.out.print("Dati valoarea: ");
-        int value=scan.nextInt();
-        array = addPosition(array, index,value);
-        System.out.print("Dimensiunea finala: " + array.length);
-        System.out.println();
-        printArray(array);
-    }    public static int[] readArray(Scanner scan, int size) {
+        System.out.println("Al doilea maxim din array: ");
+        System.out.println(largestNumber(extractElement(array, largestNumber(array))));
+        // extrage cel mai mare el. din array...
+    }
+    public static void Punctul6(Scanner scan) {
+        //Write a Java program to find the number of even and odd integers in a given array of integers
+        System.out.println("Dati dimensiune: ");
+        int dim = scan.nextInt();
+        int[] array = readArray(scan, dim);
+        System.out.println("Numere pare: " + countEven(array));
+        System.out.println("Numere impare: " + countOdd(array));
+
+    }
+
+    public static int[] readArray(Scanner scan, int size) {
+        System.out.print(" Dimensiune array");
         int[] array = new int[size];
         for (int i = 0; i < size; i++) {
-            System.out.print("array[" + (i + 1) + "]= ");
+            System.out.print("array [" + (i + 1) + "]=");
             array[i] = scan.nextInt();
         }
         return array;
-    }    public static void printArray(int[] array) {
-        System.out.print("array[]= ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-    }    public static int sumaArray(int[] array) {
+    }
+
+    public static int sumaArray(int[] array) {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum = sum + array[i];
         }
         return sum;
-    }    public static int[] extractPosition(int[] array, int index) {
-        int[] newArray = new int[array.length - 1];
-        int k = 0;
+    }
+
+    public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            if (i == index)
-                continue;
-            else {
-                newArray[k] = array[i];
-                k++;
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static int[] extractpossition(int[] array, int index) {
+        int[] newArray = new int[array.length - 1];
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            {
+                if (i == index) {
+                    continue;
+                } else {
+                    newArray[count] = array[i];
+                    count++;
+                }
             }
         }
         return newArray;
-    }    public static int[] addPosition(int[] array, int index, int value) {
+    }
+
+    public static int[] addPosition(int[] array, int index, int value) {
         int[] newArray = new int[array.length + 1];
         int k = 0;
         for (int i = 0; i < array.length; i++) {
             if (i == index) {
-                newArray[k] = value;
-                k++;
+                newArray[k] = value; // la pozitia k se insereaza noua valoare
+                k++; // incrementarea pozitiilor
             }
-            newArray[k] = array[i];
-            k++;
+            newArray[k] = array[i];// dupa inserarea elementului noul array
+            k++; //incrementarea noiilor pozitii dupa inserare deoarece sirul se muta cu o poz.
         }
         return newArray;
     }
+
+    public static int largestNumber(int[] array) {
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) { //comparam elementele din array
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int[] extractElement(int[] array, int value) {
+        int[] newArray = new int[array.length - 1];
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            {
+                if (array[i] == value) {
+                    continue;
+                } else {
+                    newArray[count] = array[i];
+                    count++;
+                }
+            }
+        }
+        return newArray;
+
+    }
+
+
+    public static int countEven(int[] array) {
+        int even = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                even++;
+            }
+        }
+        return even;
+    }
+    public static int countOdd(int[] array) {
+        return array.length - countEven(array);
+
+    }
+
 }
